7c9891a28472a39a62d7011d194dfe8fdf391116
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-04-25 19:36:56 +0500

    chore: first commit

---
+++ lib/src/application.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DocPatTrackApp extends StatelessWidget {
+  const DocPatTrackApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
