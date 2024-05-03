969efbdbe1f087068d047cfef9f9d84d88f812f2
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-05-03 16:10:17 +0500

    chore: add colors

---
+++ lib/src/shared/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+/// Colors list for current application.
+class AppColors {
+  AppColors._();
+
+  static const Color redDark = Color(0xFF7f0101);
+  static const Color blue = Color(0xFF4671c6);
+  static const Color blueGreyDark = Color(0xFF23355F);
+  static const Color blueJay = Color(0xFF3A4D7F);
+  static const Color blueMetallic = Color(0xFF51669E);
+}
