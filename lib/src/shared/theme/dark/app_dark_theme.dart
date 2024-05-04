b61d30a19f033e0d4ad000a46fdf929ea4879ed2
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-05-04 16:59:47 +0500

    chore: add scaffold background color

--- lib/src/shared/theme/dark/app_dark_theme.dart
+++ lib/src/shared/theme/dark/app_dark_theme.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import '../../../../generated/fonts.gen.dart';
+import '../app_colors.dart';
 import '../app_text_theme.dart';
 import 'app_dark_color_scheme.dart';
 import 'app_text_input_theme.dart';
 
-class AppLightTheme {
+class AppDarkTheme {
   static ThemeData get data => ThemeData(
+      scaffoldBackgroundColor: AppColors.blueGreyDark,
       colorScheme: AppDarkColorScheme.data,
       useMaterial3: true,
       inputDecorationTheme: AppLightTextInputTheme.data,
