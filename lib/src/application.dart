194416e8f4bf06859cbdd59dcb0095775b1c6f3d
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-05-05 15:14:55 +0500

    chore: update app with router

--- lib/src/application.dart
+++ lib/src/application.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
-import 'features/features.dart';
 import 'shared/shared.dart';
 
 class DocPatTrackApp extends StatelessWidget {
-  const DocPatTrackApp({super.key});
+  DocPatTrackApp({super.key});
+
+  final _router = GetIt.instance<AppRouter>();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _router.config(),
+      localizationsDelegates: AppLoc.appDelegates,
+      supportedLocales: AppLoc.supportedLocales,
       theme: AppDarkTheme.data,
       debugShowCheckedModeBanner: false,
-      home: const InitialLanguageScreen(),
+      locale: AppLoc.supportedLocales.first,
+      onGenerateTitle: (locContext) => locContext.loc.appName,
     );
   }
 }
