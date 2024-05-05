dbd426a4c1024b9d0cf3ade58e4a28210f6f6966
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-05-05 20:17:12 +0500

    chore: update navigation inner screen routes

--- lib/src/shared/navigation/app_router.dart
+++ lib/src/shared/navigation/app_router.dart
@@ -8,13 +8,30 @@ class AppRouter extends $AppRouter {
   @override
   List<AutoRoute> get routes => [
         CustomRoute(
-          guards: [LanguageGuard()],
-          initial: true,
-          page: NavigationScreenRoute.page,
-          path: '/',
-          transitionsBuilder: TransitionsBuilders.noTransition,
-          durationInMilliseconds: 0,
-        ),
+            guards: [LanguageGuard()],
+            initial: true,
+            page: NavigationScreenRoute.page,
+            path: '/',
+            transitionsBuilder: TransitionsBuilders.noTransition,
+            durationInMilliseconds: 0,
+            children: [
+              RedirectRoute(path: '', redirectTo: 'information'),
+              CustomRoute(
+                  page: InformationScreenRoute.page,
+                  path: 'information',
+                  transitionsBuilder: TransitionsBuilders.noTransition,
+                  durationInMilliseconds: 0),
+              CustomRoute(
+                  page: PatientSearchScreenRoute.page,
+                  path: 'patients',
+                  transitionsBuilder: TransitionsBuilders.noTransition,
+                  durationInMilliseconds: 0),
+              CustomRoute(
+                  page: FileTransitionScreenRoute.page,
+                  path: 'file_transition',
+                  transitionsBuilder: TransitionsBuilders.noTransition,
+                  durationInMilliseconds: 0),
+            ]),
         CustomRoute(
           page: InitialLanguageScreenRoute.page,
           path: '/language',
