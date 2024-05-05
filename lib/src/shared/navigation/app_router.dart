5c552042b603d0588cca894b5a9667179dea0b98
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-05-05 17:12:44 +0500

    chore: add guard to app router

--- lib/src/shared/navigation/app_router.dart
+++ lib/src/shared/navigation/app_router.dart
@@ -1,21 +1,25 @@
 import 'package:auto_route/auto_route.dart';
 
 import 'app_router.gr.dart';
+import 'guards/guards.dart';
 
 @AutoRouterConfig(replaceInRouteName: 'Screen,Page,Route')
 class AppRouter extends $AppRouter {
   @override
   List<AutoRoute> get routes => [
         CustomRoute(
+          guards: [LanguageGuard()],
           initial: true,
           page: NavigationScreenRoute.page,
           path: '/',
           transitionsBuilder: TransitionsBuilders.noTransition,
           durationInMilliseconds: 0,
         ),
-        AutoRoute(
+        CustomRoute(
           page: InitialLanguageScreenRoute.page,
           path: '/language',
+          transitionsBuilder: TransitionsBuilders.noTransition,
+          durationInMilliseconds: 0,
         ),
       ];
 }
