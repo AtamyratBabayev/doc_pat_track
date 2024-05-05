657e9add2bd3f11ac25abe1b96cc05d065e98297
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-05-05 14:24:08 +0500

    chore: add dark top bar theme

--- lib/src/shared/theme/dark/app_dark_theme.dart
+++ lib/src/shared/theme/dark/app_dark_theme.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import '../../../../generated/fonts.gen.dart';
 import '../app_colors.dart';
 import '../app_text_theme.dart';
+import 'app_dark_action_icons_theme.dart';
 import 'app_dark_button_theme.dart';
 import 'app_dark_color_scheme.dart';
 import 'app_dark_fab_theme.dart';
 import 'app_dark_text_input_theme.dart';
+import 'app_dark_top_bar_theme.dart';
 
 class AppDarkTheme {
   static ThemeData get data => ThemeData(
@@ -15,6 +17,8 @@ class AppDarkTheme {
       floatingActionButtonTheme: AppDarkFabTheme.data,
       filledButtonTheme: AppDarkButtonTheme.data,
       inputDecorationTheme: AppDarkTextInputTheme.data,
+      appBarTheme: AppDarkTopBarTheme.data,
+      actionIconTheme: AppDarkActionIconsTheme.data,
       fontFamily: FontFamily.gilroy,
       textTheme: AppTextTheme.data);
 }
---
+++ lib/src/shared/theme/dark/app_dark_top_bar_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../app_colors.dart';
+import '../app_text_theme.dart';
+
+class AppDarkTopBarTheme {
+  AppDarkTopBarTheme._();
+
+  static AppBarTheme get data => AppBarTheme(
+        backgroundColor: AppColors.blueGreyDark,
+        foregroundColor: Colors.white,
+        iconTheme: const IconThemeData(color: AppColors.blueKoi, size: 32.0),
+        titleTextStyle: AppTextTheme.data.headlineSmall
+            ?.copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+        elevation: 0.0,
+      );
+}
