aab30f0d79bfb51fdd3093f7ddfd54b27d88d103
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-05-05 14:26:58 +0500

    chore: add empty initial language screen

---
+++ lib/src/features/language/presentation/views/screens/initial_language/initial_language_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class InitialLanguageScreen extends StatelessWidget {
+  const InitialLanguageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Initial language screen!'),
+      ),
+    );
+  }
+}
