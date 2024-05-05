51f4a569c5e6b484d15fc4c27d9bb67b2a927674
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-05-05 20:19:03 +0500

    chore: add auto tabs router

--- lib/src/features/navigation/presentation/views/screens/navigation/navigation_screen.dart
+++ lib/src/features/navigation/presentation/views/screens/navigation/navigation_screen.dart
@@ -1,16 +1,36 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../../shared/shared.dart';
+import 'widgets/widgets.dart';
+
 @RoutePage()
 class NavigationScreen extends StatelessWidget {
   const NavigationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Navigation screen'),
-      ),
+    return AutoTabsRouter(
+      homeIndex: 0,
+      routes: const [
+        InformationScreenRoute(),
+        PatientSearchScreenRoute(),
+        FileTransitionScreenRoute()
+      ],
+      builder: (routeContext, child) {
+        final router = AutoTabsRouter.of(routeContext);
+        return Scaffold(
+          appBar: TopBar(
+            props: TopBarProps(),
+          ),
+          body: child,
+          endDrawer: AppDrawer(
+            props: AppDrawerProps(
+                position: router.activeIndex,
+                onItemPressed: router.setActiveIndex),
+          ),
+        );
+      },
     );
   }
 }
