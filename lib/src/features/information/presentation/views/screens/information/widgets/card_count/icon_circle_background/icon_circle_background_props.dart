a917131fec2c37fbd8b643cc7f0db481025fa52f
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-05-06 18:32:53 +0500

    chore: add icon circle background

---
+++ lib/src/features/information/presentation/views/screens/information/widgets/card_count/icon_circle_background/icon_circle_background_props.dart
@@ -0,0 +1,7 @@
+import '../../../../../../../../../../generated/assets.gen.dart';
+
+class IconCircleBackgroundProps {
+  const IconCircleBackgroundProps({required this.icon});
+
+  final SvgGenImage icon;
+}
