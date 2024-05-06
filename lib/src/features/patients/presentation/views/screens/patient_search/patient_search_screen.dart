29724b65670a48c5da890254307f0285ead5c0f2
Atamyrat Babayev <babayev.atamyrat@gmail.com>
2024-05-06 21:03:22 +0500

    feat: update design of patient search screen

--- lib/src/features/patients/presentation/views/screens/patient_search/patient_search_screen.dart
+++ lib/src/features/patients/presentation/views/screens/patient_search/patient_search_screen.dart
@@ -1,17 +1,132 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../../../generated/assets.gen.dart';
+import '../../../../../../shared/shared.dart';
+import '../../../../domain/domain.dart';
+import 'widgets/widgets.dart';
+
+const _horizontalPadding = 20.0;
+const _elementsPadding = 20.0;
+const _searchBarBottomPadding = 5.0;
+const _patientsPadding = 10.0;
+
 @RoutePage()
 class PatientSearchScreen extends StatelessWidget {
   const PatientSearchScreen({super.key});
 
+  void _navigateToPatient(int id) {
+    // TODO: Add implementation
+    throw UnimplementedError();
+  }
+
+  void _navigateToPatientAdd() {
+    // TODO: Add implementation
+    throw UnimplementedError();
+  }
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return ColoredBox(
-      color: theme.colorScheme.background,
-      child: const Center(
-        child: Text('Patients screen'),
+    final loc = context.loc;
+    final patients = [
+      Patient(
+          id: -1,
+          fullname: 'Allamuradow Serdar Tahirowich',
+          dateOfBirth: DateTime.now(),
+          location: 'Mary welayaty',
+          phone: '+99364010101'),
+      Patient(
+          id: -1,
+          fullname: 'Merdanow Kemal Tahirowich',
+          dateOfBirth: DateTime.now(),
+          location: 'Mary welayaty',
+          phone: '+99364010101'),
+      Patient(
+          id: -1,
+          fullname: 'Kerjanow Mered Mamedowich',
+          dateOfBirth: DateTime.now(),
+          location: 'Mary welayaty',
+          phone: '+99364010101'),
+      Patient(
+          id: -1,
+          fullname: 'Allamuradow Serdar Tahirowich',
+          dateOfBirth: DateTime.now(),
+          location: 'Mary welayaty',
+          phone: '+99364010101'),
+      Patient(
+          id: -1,
+          fullname: 'Merdanow Kemal Tahirowich',
+          dateOfBirth: DateTime.now(),
+          location: 'Mary welayaty',
+          phone: '+99364010101'),
+      Patient(
+          id: -1,
+          fullname: 'Kerjanow Mered Mamedowich',
+          dateOfBirth: DateTime.now(),
+          location: 'Mary welayaty',
+          phone: '+99364010101'),
+      Patient(
+          id: -1,
+          fullname: 'Allamuradow Serdar Tahirowich',
+          dateOfBirth: DateTime.now(),
+          location: 'Mary welayaty',
+          phone: '+99364010101'),
+      Patient(
+          id: -1,
+          fullname: 'Merdanow Kemal Tahirowich',
+          dateOfBirth: DateTime.now(),
+          location: 'Mary welayaty',
+          phone: '+99364010101'),
+      Patient(
+          id: -1,
+          fullname: 'Kerjanow Mered Mamedowich',
+          dateOfBirth: DateTime.now(),
+          location: 'Mary welayaty',
+          phone: '+99364010101'),
+    ];
+    return Scaffold(
+      floatingActionButton: Fab(
+        props:
+            FabProps(icon: Assets.icons.plus, onPressed: _navigateToPatientAdd),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: _horizontalPadding),
+            child: Text(
+              loc.patientSearchTitle,
+              style: theme.textTheme.displaySmall?.copyWith(
+                  color: theme.colorScheme.onBackground,
+                  fontWeight: FontWeight.w500),
+            ),
+          ),
+          const HeightSpacer(_elementsPadding),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: _horizontalPadding),
+            child: Input(
+              props: InputProps(
+                  hintText: loc.patientSearchHint,
+                  suffixIcon: Assets.icons.search),
+            ),
+          ),
+          const HeightSpacer(_searchBarBottomPadding),
+          Expanded(
+            child: ListView.separated(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: _horizontalPadding, vertical: _elementsPadding),
+                itemBuilder: (_, pos) => PatientCard(
+                      props: PatientCardProps(
+                          patient: patients[pos],
+                          isPrimaryAbbreviation: pos % 2 == 1,
+                          onPressed: _navigateToPatient),
+                    ),
+                separatorBuilder: (_, __) =>
+                    const HeightSpacer(_patientsPadding),
+                itemCount: patients.length),
+          )
+        ],
       ),
     );
   }
